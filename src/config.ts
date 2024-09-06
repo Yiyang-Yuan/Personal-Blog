81f845ee09a01f92c8933d45f32fc8d550c9ce9f
Yiyang-Yuan <yuanyiyang8@gmail.com>
2024-09-06 20:02:09 +1200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,16 +7,16 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Yiyang',
-  subtitle: 'Personal Blog',
+  title: 'Fuwari',
+  subtitle: 'Demo Site',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
-    hue: 200,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
   },
   banner: {
     enable: false,
-    src: 'assets/images/yiyang-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center', // Equivalent to object-position, defaults center
     credit: {
       enable: false,         // Display the credit text of the banner image
@@ -40,33 +40,33 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/Yiyang-Yuan',     // Internal links should not include the base path, as it is automatically added
+      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/yiyang-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Yiyang Yuan',
-  bio: 'Personal blog site.',
+  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: 'Lorem Ipsum',
+  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   links: [
     {
-      name: 'LinkedIn',
-      icon: 'bxl:linkedin-square',       // Visit https://icones.js.org/ for icon codes
+      name: 'Twitter',
+      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
                                         // You will need to install the corresponding icon set if it's not already included
                                         // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'www.linkedin.com/in/yyua260',
+      url: 'https://twitter.com',
     },
     {
-      name: 'Gmail',
-      icon: 'bxl:gmail',
-      url: 'yuanyiyang8@gmail.com',
+      name: 'Steam',
+      icon: 'fa6-brands:steam',
+      url: 'https://store.steampowered.com',
     },
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
-      url: 'https://github.com/Yiyang-Yuan',
+      url: 'https://github.com/saicaca/fuwari',
     },
   ],
 }
